e14f14651ddf5958e5039bfcffdfc27d62fcda66
RimuruBotMd <108895247+RimuruBotMd@users.noreply.github.com>
2022-07-16 21:51:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,12 +18,12 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6288221255023']
-global.premium = ['6288221255023']
-global.namaowner = ['NelJagoNyatir']
-global.namabot = ['NelzBot']
-global.packname = 'Created By IpanJagoNyatir'
-global.author = 'NelBotz'
+global.owner = ['6288980870067']
+global.premium = ['6288980870067']
+global.namaowner = ['SabilOfx']
+global.namabot = ['Siesta-Md']
+global.packname = 'Created By SabilOfc'
+global.author = 'Siesta-Md'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
